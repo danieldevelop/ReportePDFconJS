dee26d9b20268c1bb4c6247f346f3431731dc3fa
Daniel.Gomez <dgomez.informatica@gmail.com>
2021-03-30 01:19:27 -0300

    Se integro los datos que ingreso el usuario al pdf descargable, adicional de una pequeña validacion de campos vacios.

--- js/pdf.js
+++ js/pdf.js
@@ -1,29 +1,70 @@
-function downloadPDF() 
+function ShowSelected() 
 {
-	var doc = new jsPDF();
+    // Obtener el valor, que se encuentra en el atributo value.
+    let valor = document.getElementById("inpTipoDeViaje").value.trim();
+    
+    // Obtener el texto, que ve el usuario.
+    // var comboBox = document.getElementById("inpTipoDeViaje");
+    // var selected = comboBox.options[comboBox.selectedIndex].text;
+    // console.info(selected);
 
-	// doc.text(20, 20, 'Hello landscape world!'); // doc.text(width, height, <text>)
-	// doc.text(20, 20, document.getElementById("inpSalida").value.trim());
-	
-	doc.setFontSize(16);
-	doc.text(80, 15, 'Ventas de Vuelos.');
-	doc.setLineWidth(0.1);
-	doc.line(20, 20, 190, 20); // horizontal line (3 parametro en el width)
-
-	
-
-
-
-	// Optional - set properties on the document
-	doc.setProperties({
-    	title: 'Reporte de Venta',
-    	subject: 'This is the subject',
-    	author: 'James Hall',
-    	keywords: 'generated, javascript, web 2.0, ajax',
-    	creator: 'MEEE'
-	});
-
-	// Output as Data URI
-	doc.save('Reporte de Venta.pdf');
+    return valor;
 }
 
+function downloadPDF(puntoDeSalida, destino, tipoDeViaje) 
+{
+    var doc = new jsPDF();
+    var mes = new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto",
+        "Septiembre","Octubre","Noviembre","Diciembre");
+    var f = new Date(document.getElementById("inpFechaDeViaje").value.trim());
+
+    // Optional - set properties on the document
+    doc.setProperties({
+        title: 'Reporte de Venta',
+        subject: 'This is the subject',
+        author: 'James Hall',
+        keywords: 'generated, javascript, web 2.0, ajax',
+        creator: 'MEEE'
+    });
+
+    var posicion = 15;
+    doc.text(80, posicion, 'Ventas de Vuelos.');
+    doc.setFontSize(16);
+    doc.setLineWidth(0.1);
+    doc.line(20, 20, 190, 20); // horizontal line (3 parametro en el width)
+    posicion += 15;
+
+    doc.text(20, posicion, `Punto de Salida: ${puntoDeSalida}`);
+    posicion += 8;
+    doc.text(20, posicion, `Destino: ${destino}`);
+    posicion += 8;
+    doc.text(20, posicion, `Tipo de Viaje: ${tipoDeViaje.toUpperCase()} - Vida Silvestre.`);
+    posicion += 8;
+    doc.text(20, posicion, `Fecha de Viaje: ${f.getDate()+1} de ${mes[f.getMonth()]} del ${f.getFullYear()}`);
+    
+
+    // Output as Data URI
+    doc.save('Reporte de Venta.pdf');
+}
+
+
+
+
+document.getElementById("btnGenerarPDF").addEventListener('click', function() {
+    let puntoDeSalida = document.getElementById("inpSalida").value.trim();
+    let destino = document.getElementById("inpDestino").value.trim();
+    let tipoDeViaje = ShowSelected();
+    let fechaDeViaje = document.getElementById("inpFechaDeViaje").value.trim();
+
+    if (puntoDeSalida == "" || destino == "" || tipoDeViaje == "" || fechaDeViaje == "") {
+        alert("Todos los campos son obligatorios.");
+        return false;
+    }
+
+    downloadPDF(puntoDeSalida, destino, tipoDeViaje);
+    // Limpiamos los campos despues de descargar el pdf
+    document.getElementById("inpSalida").value = "";
+    document.getElementById("inpDestino").value = "";
+    document.getElementById("inpTipoDeViaje").value = "";
+    document.getElementById("inpFechaDeViaje").value = "";
+});
